3d4bd38b9ff52c83055b75b65c0083939c56fc95
Viezurele <luca.radut@gmail.com>
2017-03-22 15:34:18 +0200

    Add files via upload

--- user.php
+++ user.php
@@ -1,20 +1,20 @@
 <?php
 
-$con = mysql_connect("pp.sula.ro","sula_ncoaste","Muist!1523");
+$con = mysql_connect("pp.sula.ro","sula_guest","guest1523");
 if (!$con)
   {
   die('Could not connect: ' . mysql_error());
   }
   mysql_select_db("sula_pp", $con);
 
-$sql="INSERT INTO login(User,mail)
-VALUES
-('$_POST[user]','$_POST[pass]')";
-
+$sql="INSERT INTO profile(name,tasks,habits)
+VALUES ('$_POST[user]','$_POST[pass]','$_POST[prop]')";
+ 
 if (!mysql_query($sql,$con))
   {
   die('Error: ' . mysql_error());
   }
+
 echo "1 record added";
 
 mysql_close($con)
